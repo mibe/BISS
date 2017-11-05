59cd157920e0a50926ae7cedcae6b74e6533ff05
Michael Bemmerl <mail@mx-server.de>
2017-11-05 03:20:38 +0100

    Implement the timeout logic in the blinker code.

--- Hardware/Blinky/firmware/Blinker.c
+++ Hardware/Blinky/firmware/Blinker.c
@@ -1,6 +1,8 @@
 #include "Blinker.h"
 
-static uint8_t counter;
+static uint8_t blinkCounter;
+static uint8_t ovrflwToSec;
+static uint16_t secondsCounter;
 
 static void _display_enable(void)
 {
@@ -16,7 +18,9 @@ static void _display_disable(void)
 
 void Blinker_Enable(uint8_t enableTouchSensor)
 {
-	counter = 0;
+	blinkCounter = 0;
+	ovrflwToSec = 0;
+	secondsCounter = 0;
 	
 	// Normal mode, with prescaler of 1024; overflow interrupt enabled
 	TCCR0B |= _BV(CS02) | _BV(CS00);
@@ -42,10 +46,30 @@ void Blinker_Disable(void)
 ISR(TIMER0_OVF_vect)
 {
 	// Overflow of Timer0 happened. Increment our internal counter.
-	counter++;
+	blinkCounter++;
+	
+	// Timeout logic: 16.000.000 MHz F_CPU with a timer prescaler of 1024 and
+	// an overflow of an 8 bit timer results in 61.03 interrupts per second.
+	// The error of 0.0351... can be neglected here.
+	if (++ovrflwToSec == 61)
+	{
+		// Increment the seconds counter and reset the overflow counter.
+		secondsCounter++;
+		ovrflwToSec = 0;
+		
+		// The maximum timeout is 300 seconds. But the timeout value is stored
+		// in an 1 byte variable. This means the minimum timeout is 300-256=44
+		// seconds. This has to be considered here.
+		if (secondsCounter > 44)
+			if (secondsCounter - 44 > settings.BlinkTimeout)
+			{
+				Blinker_Disable();
+				return;
+			}
+	}
 	
 	// If the setpoint is reached, toggle the display.
-	if (counter >= settings.BlinkInterval)
+	if (blinkCounter >= settings.BlinkInterval)
 	{
 		// Bit 0 of GPIO register 0 is used as a status bit to check whether
 		// the display is enabled or not.
@@ -56,7 +80,7 @@ ISR(TIMER0_OVF_vect)
 		
 		// Reset internal counter.
 		// This makes it practically a timer in CTC mode with a larger prescaler.
-		counter = 0;
+		blinkCounter = 0;
 	}
 	
 	// Check if the touch sensor is enabled and if yes, check if it sensed a touch.
