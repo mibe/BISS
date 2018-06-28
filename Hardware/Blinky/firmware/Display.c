14a7f9a253ee4acd188f9899c490d5b3177b9e6c
Michael Bemmerl <mail@mx-server.de>
2018-06-28 21:17:13 +0200

    Someone (read: I) forgot the pull-down resistors at the gates at the MOSFETs. 😳
    This bug is fixed in software: The timer which provides the PWM signal to the gate is stopped when the LED should be off and started again when it should be on again. The pin is at all times configured as output.

--- Hardware/Blinky/firmware/Display.c
+++ Hardware/Blinky/firmware/Display.c
@@ -9,7 +9,11 @@ void Display_Setup(void)
 	// Fast PWM, 8-bit, clear on match, set on top, with prescaler of 1024
 	TCCR1A |= _BV(COM1A1) | _BV(COM1B1) | _BV(COM1C1) | _BV(WGM10);
 	TCCR1B |= _BV(WGM12) | _BV(CS12) | _BV(CS10);
-
+	
+	// Set the pins as outputs (this also puts a low signal to the pin)
+	DDRC |= _BV(PC6) | _BV(PC5);
+	DDRB |= _BV(PB7);
+	
 	Display_Update();
 }
 
@@ -19,33 +23,25 @@ void Display_Update()
 	OCR1B = settings.Color.G;
 	OCR1C = settings.Color.B;
 	
-	// Disable the outputs if no PWM is required.
-	if (settings.Color.R == 0)
-		DDRC &= ~_BV(PC6);
-	
-	if (settings.Color.G == 0)
-		DDRC &= ~_BV(PC5);
-	
-	if (settings.Color.B == 0)
-		DDRB &= ~_BV(PB7);
+	// Enable / disable the display depending if the LED should be active
+	if (OCR1A > 0 || OCR1B > 0 || OCR1C > 0)
+		Display_Enable();
+	else if (OCR1A == 0 && OCR1B == 0 && OCR1C == 0)
+		Display_Disable();
 }
 
 void Display_Enable(void)
 {
-	// Set the port pins as outputs if the output compare registers are set.
-	if (OCR1A > 0)
-		DDRC |= _BV(PC6);
-	
-	if (OCR1B > 0)
-		DDRC |= _BV(PC5);
-	
-	if (OCR1C > 0)
-		DDRB |= _BV(PB7);
+	// Start the timer
+	TCCR1B |= _BV(CS12) | _BV(CS10);
 }
 
 void Display_Disable(void)
 {
-	// Disable all outputs
-	DDRC &= ~_BV(PC6) & ~_BV(PC5);
-	DDRB &= ~_BV(PB7);
+	// Stop the timer
+	TCCR1B &= ~(_BV(CS10) | _BV(CS12));
+	
+	OCR1A = 0;
+	OCR1B = 0;
+	OCR1C = 0;
 }
